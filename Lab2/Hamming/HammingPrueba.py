d32b9329d2e442aa5c9163829425a1a6d26987cb
Javier Mombiela <61723252+javim7@users.noreply.github.com>
2023-08-09 15:29:20 -0600

    Update HammingPrueba.py

--- Lab2/Hamming/HammingPrueba.py
+++ Lab2/Hamming/HammingPrueba.py
@@ -1,6 +1,6 @@
 import subprocess
 import os
 
-subprocess.run(["python", "Hamming2.py"])
-subprocess.run(["java", "Hamming2.java"])
+# subprocess.run(["python", "Lab2/Hamming/Hamming2.py"])
+subprocess.run(["javac", "Lab2/Hamming/Hamming2.java"])
 # subprocess.run(["java", "Lab2\Hamming\Hamming2"])
